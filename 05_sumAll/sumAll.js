5781a5000e465bbaaf2d6ae03de956262718cd3c
Huot Rithy <huotrithy01@gmail.com>
2023-12-22 18:46:10 +0700

    Clean up 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
 const sumAll = function(num1, num2) {
     
-    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+
+    if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
 
@@ -8,6 +12,9 @@ const sumAll = function(num1, num2) {
     let startNum;
     let endNum;
     num1 > num2 ? (startNum = num2, endNum = num1) : (startNum = num1, endNum = num2); 
+
+    // Better and more readable way to swap value
+    // if (min > max) [min, max] = [max, min];
 
     for (let i = startNum; i <= endNum; i++) {
         sum += i;
