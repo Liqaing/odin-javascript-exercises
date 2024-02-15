f46710e54d25ad0c4836f35975ee11fb393dd903
Rithy <huotrithy01@gmail.com>
2024-02-15 21:47:46 +0700

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  arr.forEach(element => {
+    mul *= element;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
