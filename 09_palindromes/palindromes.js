47235c182e29ba721740c072e3b1cbbef97c3e10
Rithy <huotrithy01@gmail.com>
2024-02-15 22:25:46 +0700

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+    let str = inputStr.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let reverseStr = str.split("").reverse().join("");
+    // console.log(reverseStr)
 
+    if (str == reverseStr) {
+        return true;
+    }
+    return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
