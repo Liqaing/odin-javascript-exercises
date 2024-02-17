ac01db22d62d84ec1abc13b3b12b2e893903687e
Rithy <huotrithy01@gmail.com>
2024-02-17 20:25:20 +0700

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    try {
+        num = parseInt(num);
+    }
+    catch {
+        return;
+    }
 
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        if (i === 0 || i === 1) {
+            arr[i] = 1;
+        }
+        else {
+            let value = arr[i - 1] + arr[i - 2];
+            arr[i] = value;
+        }
+    }
+    return arr[num - 1];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
