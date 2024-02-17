0f14e13cdeb5c27d54324c2d6cad2f5f83bcbaba
Rithy <huotrithy01@gmail.com>
2024-02-17 21:05:43 +0700

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    people.sort((a, b) => {
+        if (a.hasOwnProperty("yearOfDeath")) {
+            personAAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        else {
+            personAAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        
+        if (b.hasOwnProperty("yearOfDeath")) {
+            personBAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            personBAge = new Date().getFullYear() - b.yearOfBirth;
+        }
 
+        return personBAge - personAAge;
+    });
+
+    return people[0] 
 };
 
 // Do not edit below this line
